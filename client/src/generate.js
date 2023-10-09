350adbcb9a9c1130f05633a99640f1b22ff2237d
ouckah <aidan.ouckama@gmail.com>
2023-10-08 20:47:25 -0400

    ✨ Endpoint Attempt

--- client/src/generate.js
+++ client/src/generate.js
@@ -1,7 +1,42 @@
-import Webcam from "react-webcam";
+// import Webcam from "react-webcam";
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import { useState } from "react";
 
 function Generate() {
+
+  const [emotion, setEmotion] = useState("");
+
+  const getEmotion = async () => {
+    try {
+      const response = await axios.get("/analyze_emotion");
+  
+      // Handle the response data as needed
+      console.log(response.data);
+
+      const emotions = response.data["emotion"];
+      const maxEmotion = Math.max(emotions["happy"], emotions["sad"], emotions["angry"]);
+
+      switch (maxEmotion) {
+        case emotions["happy"]:
+          setEmotion("Happy");
+          break;
+        case emotions["sad"]:
+          setEmotion("Sad");
+          break;
+        case emotions["angry"]:
+          setEmotion("Angry");
+          break;
+        default:
+          setEmotion("");
+          break;
+      }
+    } catch (error) {
+      // Handle any errors that occur during the request
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <>
     
@@ -10,12 +45,12 @@ function Generate() {
             <div className='w-full h-1/6'/>
             <div className="flex flex-row justify-center items-center w-full h-2/3 bg-black">
                 <div className="w-2/3 h-full">
-                    <Webcam width={1135} height={540} />
+                  <img src="{{ url_for('video') }}" width="50%" />
                 </div>
-                <div className="flex flex-col justify-center items-center w-1/3 h- gap-12">
+                <div className="flex flex-col justify-center items-center w-1/3 h-full gap-12">
                     <div className="flex flex-col justify-center items-center gap-2">
                         <h1 className='lg:text-7xl text-5xl text-white font-bold uppercase transition duration-300 hover:drop-shadow-lg'>Emotion:</h1>
-                        <p className='lg:text-5xl text-3xl text-white font-bold transition duration-300 hover:drop-shadow-lg'>Happy</p>
+                        <p className='lg:text-5xl text-3xl text-white font-bold transition duration-300 hover:drop-shadow-lg'>{emotion}</p>
                     </div>
 
                     <Link to="/generate">
